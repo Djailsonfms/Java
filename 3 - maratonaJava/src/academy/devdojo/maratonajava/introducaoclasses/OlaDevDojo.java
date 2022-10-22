720b87b97c7a9597945ea3b6ccfa13b33c3c5696
Djailson F M Silva <djailsonfms@gmail.com>
2022-10-22 13:24:41 -0300

    Atualização do dia 22/10/2020

---
+++ 3 - maratonaJava/src/academy/devdojo/maratonajava/introducaoclasses/OlaDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducaoclasses;
+
+public class OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("Ka-ME-HA-ME-HA");
+    }
+}
